eabe9701486a9333b741d355e23bc9070d87402d
Saul Canales <lanced2014@gmail.com>
2025-06-26 20:28:13 -0500

    Agregar multiplicación y división

--- calculadora.js
+++ calculadora.js
@@ -8,3 +8,12 @@ function restar(a, b) {
 
 sumar (5,3);
 restar(10,4);
+
+function multiplicar(a, b) {
+  return a * b;
+}
+
+function dividir(a, b) {
+  if (b === 0) throw new Error("División por cero");
+  return a / b;
+}
